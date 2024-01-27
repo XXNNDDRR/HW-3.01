b57445a6a3b7536b16ee4f52503bf4a997c7a4ce
XXNNDDRR <xndrgo@gmail.com>
2024-01-27 19:26:19 +0700

    Run!

---
+++ HW-3.01/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  HW-3.01
+//
+//  Created by Alexander on 27.01.2024.
+//
+
+import Foundation
---
+++ HW-3.01/Services/SpringAnimationStore.swift
@@ -0,0 +1,76 @@
+//
+//  SpringInfoStore.swift
+//  HW-3.01
+//
+//  Created by Alexander on 27.01.2024.
+//
+
+import Foundation
+
+final class SpringAnimationStore {
+    static let shared = SpringAnimationStore()
+    
+    let animations = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+    ]
+    
+    let curves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
+    
+    private init() {}
+}
